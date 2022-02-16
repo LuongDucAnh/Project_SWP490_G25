2c25f8014ca9ffe2ca91fc8a58c39c3cac415637
Nguyễn Trung Thành <97335017+ThanhNT130929@users.noreply.github.com>
2022-02-17 06:04:19 +0700

    commit update code

---
+++ swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/model/course.java
@@ -0,0 +1,70 @@
+package com.example.swp490_g25_sse.model;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Course")
+public class course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int courseId;
+
+    @Column(name = "courseName")
+    private String courseName;
+
+    @Column(name = "ratingId")
+    private int ratingId;
+
+    @Column(name = "createDate")
+    private Date createDate;
+
+    @Column(name = "updateDate")
+    private Date updateDate;
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public int getRatingId() {
+        return ratingId;
+    }
+
+    public void setRatingId(int ratingId) {
+        this.ratingId = ratingId;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+}
