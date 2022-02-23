589bef43857d5b3ba866768600f89a88ecd7a2dd
HuyPXHE140702 <huypxhe140702@fpt.edu.vn>
2022-02-24 00:24:52 +0700

    commit createCourse.html

--- swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/controller/MainController.java
+++ swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/controller/MainController.java
@@ -4,10 +4,23 @@
  */
 package com.example.swp490_g25_sse.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 /**
  *
  * @author ADMIN
  */
+@Controller
 public class MainController {
-    
+
+    @GetMapping("/")
+    private String home() {
+        return "index";
+    }
+
+    @GetMapping("/login")
+    private String loginController() {
+        return "login";
+    }
 }
