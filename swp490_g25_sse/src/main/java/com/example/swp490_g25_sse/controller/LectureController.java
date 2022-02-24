20f0a0e4b09bdace3a771954d8bb54ed2cfd3a30
LuongDucAnh <85573042+LuongDucAnh@users.noreply.github.com>
2022-02-24 10:31:38 +0700

    Add LectureController getAllCourseLectures method

--- swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/controller/LectureController.java
+++ swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/controller/LectureController.java
@@ -4,10 +4,27 @@
  */
 package com.example.swp490_g25_sse.controller;
 
+import com.example.swp490_g25_sse.model.CourseLecture;
+import com.example.swp490_g25_sse.repository.LectureRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author ADMIN
  */
+@RestController
+@RequestMapping("/lectures")
 public class LectureController {
     
+    @Autowired
+    private LectureRepository lectureRepository;
+
+    @GetMapping("")
+    public List<CourseLecture> getAllCourseLectures() {
+        return lectureRepository.findAll();
+    }
 }
