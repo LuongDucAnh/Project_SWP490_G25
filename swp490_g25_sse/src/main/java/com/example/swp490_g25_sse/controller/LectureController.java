bfd3aefc1dd4467ab6f3eaff5f77f0c6b082013f
LuongDucAnh <85573042+LuongDucAnh@users.noreply.github.com>
2022-02-24 10:35:03 +0700

    Add LectureController getCourseLectureById method and ResourceNotFoundException

--- swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/controller/LectureController.java
+++ swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/controller/LectureController.java
@@ -4,11 +4,14 @@
  */
 package com.example.swp490_g25_sse.controller;
 
+import com.example.swp490_g25_sse.exception.ResourceNotFoundException;
 import com.example.swp490_g25_sse.model.CourseLecture;
 import com.example.swp490_g25_sse.repository.LectureRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +29,13 @@ public class LectureController {
     @GetMapping("")
     public List<CourseLecture> getAllCourseLectures() {
         return lectureRepository.findAll();
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<CourseLecture> getCourseLectureById(@PathVariable(value = "id") int CourseLectureId)
+            throws ResourceNotFoundException, Exception {
+        CourseLecture CourseLecture = lectureRepository.findById(CourseLectureId)
+                .orElseThrow(() -> new Exception("CourseLecture not found for this id :: " + CourseLectureId));
+        return ResponseEntity.ok().body(CourseLecture);
     }
 }
