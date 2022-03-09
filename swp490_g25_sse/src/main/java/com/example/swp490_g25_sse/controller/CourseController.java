c1ba9757f655ae15cc8eb2857b6c3d4d0faad044
HuyPXHE140702 <huypxhe140702@fpt.edu.vn>
2022-03-09 16:28:58 +0700

    Update CourseController.java

--- swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/controller/CourseController.java
+++ swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/controller/CourseController.java
@@ -8,9 +8,7 @@ import com.example.swp490_g25_sse.model.Course;
 import com.example.swp490_g25_sse.model.CourseDescription;
 import com.example.swp490_g25_sse.model.CourseImage;
 import com.example.swp490_g25_sse.repository.CourseRepository;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -19,7 +17,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  *
