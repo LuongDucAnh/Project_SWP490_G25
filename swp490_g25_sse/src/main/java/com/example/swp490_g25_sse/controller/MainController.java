7d666a1eeea83f66f06e36cc37e92ef78fed8864
HuyPXHE140702 <huypxhe140702@fpt.edu.vn>
2022-03-12 16:13:21 +0700

    Update MainController.java

--- swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/controller/MainController.java
+++ swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/controller/MainController.java
@@ -4,8 +4,16 @@
  */
 package com.example.swp490_g25_sse.controller;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 /**
  *
@@ -23,4 +31,67 @@ public class MainController {
     private String loginController() {
         return "login";
     }
+
+    @GetMapping("/app")
+    private String app() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if (!(auth instanceof AnonymousAuthenticationToken)) {
+            if (auth.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_TEACHER"))) {
+                return "redirect:/app/teacher";
+            } else if (auth.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_STUDENT"))) {
+                return "redirect:/app/student";
+            } else {
+                return "login";
+            }
+
+        } else {
+            return "login";
+        }
+    }
+
+//    @GetMapping("/account-info")
+//    private String accountInfo(Model model) {
+//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//        CustomUserDetailsService userDetails = (CustomUserDetailsService) auth.getPrincipal();
+//        UserInfoDto userInfo = new UserInfoDto(userDetails.getUser().getFirstName(),
+//                userDetails.getUser().getLastName(), userDetails.getUser().getEmail());
+//
+//        model.addAttribute("userInfo", userInfo);
+//
+//        return "account-info";
+//    }
+//
+//    @PostMapping("/account-info")
+//    private String updateUserInfo(@ModelAttribute("userInfo") UserInfoDto userInfo) {
+//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//        CustomUserDetailsService currentUser = (CustomUserDetailsService) auth.getPrincipal();
+//        if (userInfo.getEmail().equals("") || userInfo.getEmail() == null) {
+//            System.out.println("Error");
+//        }
+//
+//        userService.updateInfo(userInfo, currentUser.getUser());
+//        return "redirect:/account-info";
+//    }
+//
+//    @GetMapping("/account-setting")
+//    private String accountSetting(Model model) {
+//        AccountInfoDto accountInfo = new AccountInfoDto();
+//        model.addAttribute("accountInfo", accountInfo);
+//        return "account-setting";
+//    }
+//
+//    @PostMapping("/account-setting")
+//    private String updateAccountInfo(@ModelAttribute("accountInfo") AccountInfoDto accountInfo) {
+//        try {
+//            userService.updateAccount(accountInfo);
+//        } catch (ResponseStatusException e) {
+//            System.out.println(e.getMessage());
+//            return "redirect:/account-setting?error=".concat(e.getReason());
+//        }
+//
+//        System.out.println("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDd");
+//
+//        return "redirect:/account-setting";
 }
+
