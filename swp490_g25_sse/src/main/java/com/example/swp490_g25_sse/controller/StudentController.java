65fd03ffd7014c49384f861347431ce268cff9ad
HuyPXHE140702 <huypxhe140702@fpt.edu.vn>
2022-03-24 18:56:08 +0700

    update StudentController, course-overview.html

--- swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/controller/StudentController.java
+++ swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/controller/StudentController.java
@@ -1,7 +1,10 @@
 package com.example.swp490_g25_sse.controller;
 
+import com.example.swp490_g25_sse.model.Course;
 import com.example.swp490_g25_sse.model.User;
+import com.example.swp490_g25_sse.service.CourseService;
 import com.example.swp490_g25_sse.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -9,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /*
@@ -23,6 +27,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/app/student")
 public class StudentController {
 
+    @Autowired
+    private CourseService courseService;
+    
     @GetMapping("")
     private String index(Model model) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
@@ -30,5 +37,19 @@ public class StudentController {
         
         model.addAttribute("userName", userDetails.getUser().getFirstName());
         return "course-screen";
+    }
+    
+        @GetMapping("/course/{id}")
+    private String courseOverview(@PathVariable String id, Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        CustomUserDetailsService userDetails = (CustomUserDetailsService) auth.getPrincipal();
+
+        Course course = courseService.getCourseById(Long.parseLong(id)).get();
+
+        model.addAttribute("userName", userDetails.getUser().getFirstName());
+        model.addAttribute("course", course);
+
+        // System.out.println(top4Course.getContent().get(0).getImageUrl());
+        return "student/course-overview";
     }
 }
