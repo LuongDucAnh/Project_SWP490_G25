556331841cba5f01a3c05ab56e49ec85bb04d46f
LuongDucAnh <85573042+LuongDucAnh@users.noreply.github.com>
2022-03-30 06:06:57 +0700

    Update list course

--- swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/repository/CourseRepository.java
+++ swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/repository/CourseRepository.java
@@ -5,6 +5,8 @@
 package com.example.swp490_g25_sse.repository;
 
 import com.example.swp490_g25_sse.model.Course;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -15,5 +17,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long>{
-    
+    Page<Course> findAll(Pageable pageable);
 }
