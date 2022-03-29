2e0305fb3310ea5ff52641962da331b2a095f09c
HuyPXHE140702 <huypxhe140702@fpt.edu.vn>
2022-03-29 22:49:32 +0700

    Update StudentController.java

--- swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/controller/StudentController.java
+++ swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/controller/StudentController.java
@@ -21,10 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-/**
- *
- * @author msi
- */
+
 @Controller
 @RequestMapping("/app/student")
 public class StudentController {
