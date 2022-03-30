eb0d85dad1bba3d734fe0fec8c322a0924d3fde9
HuyPXHE140702 <huypxhe140702@fpt.edu.vn>
2022-03-30 19:07:05 +0700

    Update account info, account setting

--- swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/controller/MainController.java
+++ swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/controller/MainController.java
@@ -4,6 +4,11 @@
  */
 package com.example.swp490_g25_sse.controller;
 
+import com.example.swp490_g25_sse.dto.AccountInfoDto;
+import com.example.swp490_g25_sse.dto.UserInfoDto;
+import com.example.swp490_g25_sse.service.CustomUserDetailsService;
+import com.example.swp490_g25_sse.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -21,6 +26,9 @@ import org.springframework.web.server.ResponseStatusException;
  */
 @Controller
 public class MainController {
+
+    @Autowired
+    UserServiceImpl userService;
 
     @GetMapping("/")
     private String home() {
@@ -50,48 +58,48 @@ public class MainController {
         }
     }
 
-//    @GetMapping("/account-info")
-//    private String accountInfo(Model model) {
-//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-//        CustomUserDetailsService userDetails = (CustomUserDetailsService) auth.getPrincipal();
-//        UserInfoDto userInfo = new UserInfoDto(userDetails.getUser().getFirstName(),
-//                userDetails.getUser().getLastName(), userDetails.getUser().getEmail());
-//
-//        model.addAttribute("userInfo", userInfo);
-//
-//        return "account-info";
-//    }
-//
-//    @PostMapping("/account-info")
-//    private String updateUserInfo(@ModelAttribute("userInfo") UserInfoDto userInfo) {
-//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-//        CustomUserDetailsService currentUser = (CustomUserDetailsService) auth.getPrincipal();
-//        if (userInfo.getEmail().equals("") || userInfo.getEmail() == null) {
-//            System.out.println("Error");
-//        }
-//
-//        userService.updateInfo(userInfo, currentUser.getUser());
-//        return "redirect:/account-info";
-//    }
-//
-//    @GetMapping("/account-setting")
-//    private String accountSetting(Model model) {
-//        AccountInfoDto accountInfo = new AccountInfoDto();
-//        model.addAttribute("accountInfo", accountInfo);
-//        return "account-setting";
-//    }
-//
-//    @PostMapping("/account-setting")
-//    private String updateAccountInfo(@ModelAttribute("accountInfo") AccountInfoDto accountInfo) {
-//        try {
-//            userService.updateAccount(accountInfo);
-//        } catch (ResponseStatusException e) {
-//            System.out.println(e.getMessage());
-//            return "redirect:/account-setting?error=".concat(e.getReason());
-//        }
-//
-//        System.out.println("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDd");
-//
-//        return "redirect:/account-setting";
-}
+    @GetMapping("/account-info")
+    private String accountInfo(Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        CustomUserDetailsService userDetails = (CustomUserDetailsService) auth.getPrincipal();
+        UserInfoDto userInfo = new UserInfoDto(userDetails.getUser().getFirstName(),
+                userDetails.getUser().getLastName(), userDetails.getUser().getEmail());
 
+        model.addAttribute("userInfo", userInfo);
+
+        return "account-info";
+    }
+
+    @PostMapping("/account-info")
+    private String updateUserInfo(@ModelAttribute("userInfo") UserInfoDto userInfo) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        CustomUserDetailsService currentUser = (CustomUserDetailsService) auth.getPrincipal();
+        if (userInfo.getEmail().equals("") || userInfo.getEmail() == null) {
+            System.out.println("Error");
+        }
+
+        userService.updateInfo(userInfo, currentUser.getUser());
+        return "redirect:/account-info";
+    }
+
+    @GetMapping("/account-setting")
+    private String accountSetting(Model model) {
+        AccountInfoDto accountInfo = new AccountInfoDto();
+        model.addAttribute("accountInfo", accountInfo);
+        return "account-setting";
+    }
+
+    @PostMapping("/account-setting")
+    private String updateAccountInfo(@ModelAttribute("accountInfo") AccountInfoDto accountInfo) {
+        try {
+            userService.updateAccount(accountInfo);
+        } catch (ResponseStatusException e) {
+            System.out.println(e.getMessage());
+            return "redirect:/account-setting?error=".concat(e.getReason());
+        }
+
+        System.out.println("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDd");
+
+        return "redirect:/account-setting";
+    }
+}
