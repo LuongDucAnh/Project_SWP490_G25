ba5bf666df63966d9fda9385a6453c7136dafc42
HuyPXHE140702 <huypxhe140702@fpt.edu.vn>
2022-04-07 19:06:16 +0700

    update studentService, studentServiceImpl

---
+++ swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/service/StudentServiceImpl.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.swp490_g25_sse.service;
+
+import com.example.swp490_g25_sse.model.Student;
+import com.example.swp490_g25_sse.model.User;
+import com.example.swp490_g25_sse.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Admin
+ */
+@Service
+public class StudentServiceImpl implements StudentService {
+
+	@Autowired
+	private StudentRepository studentRepository;
+
+
+  @Override
+  public Student getStudentInfo(User user) {
+    Student student = studentRepository.findFirstByUserId(user.getId());
+
+    return student;
+  }
+
+}
