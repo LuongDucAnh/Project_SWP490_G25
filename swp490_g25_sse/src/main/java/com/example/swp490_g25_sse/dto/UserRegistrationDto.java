a6c9884b16d632386c46e7622199884bba1c51b6
BinhNguyen9x <binhnnse04190@fpt.edu.vn>
2022-04-10 09:27:41 +0700

    Update UserRegistrationDto.java

--- swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/dto/UserRegistrationDto.java
+++ swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/dto/UserRegistrationDto.java
@@ -34,6 +34,32 @@ public class UserRegistrationDto {
         return lastName;
     }
 
-   
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 
 }
