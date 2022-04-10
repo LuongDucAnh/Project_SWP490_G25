d8966ae63d8480d465a52a8edcb258b5c8a37147
BinhNguyen9x <binhnnse04190@fpt.edu.vn>
2022-04-10 09:25:36 +0700

    Update UserRegistrationDto.java

--- swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/dto/UserRegistrationDto.java
+++ swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/dto/UserRegistrationDto.java
@@ -1,13 +1,6 @@
 package com.example.swp490_g25_sse.dto;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-/**
- *
- * @author bettafish15
- */
+
 public class UserRegistrationDto {
 
     private String firstName;
@@ -41,32 +34,6 @@ public class UserRegistrationDto {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
+   
 
 }
