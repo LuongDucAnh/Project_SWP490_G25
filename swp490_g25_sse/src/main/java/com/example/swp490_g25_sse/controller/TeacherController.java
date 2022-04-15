9faf8e8d3a30ef9ac8a961845036667efd574650
HuyPXHE140702 <huypxhe140702@fpt.edu.vn>
2022-04-15 23:01:03 +0700

    Update TeacherController.java

--- swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/controller/TeacherController.java
+++ swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/controller/TeacherController.java
@@ -18,14 +18,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-/**
- *
- * @author msi
- */
 @Controller
 @RequestMapping("/app/teacher")
 public class TeacherController {
