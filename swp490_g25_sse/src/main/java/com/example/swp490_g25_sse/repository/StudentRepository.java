2773e6d2029dbc03d70d53ce1f5a9a940652dbb0
HuyPXHE140702 <huypxhe140702@fpt.edu.vn>
2022-04-17 00:44:56 +0700

    Update StudentRepository.java

--- swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/repository/StudentRepository.java
+++ swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/repository/StudentRepository.java
@@ -12,5 +12,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @author Admin
  */
 public interface StudentRepository extends JpaRepository<Student, Long> {
-  Student findFirstByUserId(long id);
+  Student findFirstByUserId(long id);//
 }
