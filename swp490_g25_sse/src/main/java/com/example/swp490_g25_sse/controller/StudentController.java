9a26831b05ea185311cc1b3271b0487a7f1443c9
Nguyễn Trung Thành <97335017+ThanhNT130929@users.noreply.github.com>
2022-04-20 00:05:38 +0700

    Update StudentController.java

--- swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/controller/StudentController.java
+++ swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/controller/StudentController.java
@@ -1,17 +1,22 @@
 package com.example.swp490_g25_sse.controller;
 
+import java.io.OutputStream;
+import java.util.Collection;
+import java.util.List;
+
 import com.example.swp490_g25_sse.dto.CourseDto;
 import com.example.swp490_g25_sse.dto.CourseOverviewDto;
 import com.example.swp490_g25_sse.dto.FeedbackDto;
-import java.util.List;
-
+import com.example.swp490_g25_sse.dto.MilestoneDto;
 import com.example.swp490_g25_sse.model.Course;
+import com.example.swp490_g25_sse.model.Feedback;
 import com.example.swp490_g25_sse.model.Student;
 import com.example.swp490_g25_sse.model.StudentCourseEnrollment;
 import com.example.swp490_g25_sse.service.CourseService;
 import com.example.swp490_g25_sse.service.StudentService;
 import com.example.swp490_g25_sse.service.TestResultService;
 import com.example.swp490_g25_sse.service.CustomUserDetailsService;
+import com.example.swp490_g25_sse.service.FeedbackService;
 import com.example.swp490_g25_sse.service.LectureResultService;
 import com.example.swp490_g25_sse.service.StudentCourseEnrollmentService;
 
@@ -31,12 +36,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author msi
+ */
 @Controller
 @RequestMapping("/app/student")
 public class StudentController {
 
     @Autowired
     private Environment env;
+
+    @Autowired
+    private FeedbackService feedbackService;
 
     @Autowired
     private CourseService courseService;
@@ -73,12 +89,51 @@ public class StudentController {
 
     @GetMapping("/registeredCourses")
     private String registeredCourses(Model model, @RequestParam(name = "isFinished") Boolean isFinished) {
+        if (isFinished == null) {
+            isFinished = true;
+        }
+
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         CustomUserDetailsService userDetails = (CustomUserDetailsService) auth.getPrincipal();
 
         Student student = studentService.getStudentInfo(userDetails.getUser());
+        String contentStr = "";
+        String[] contentPart;
+        char lastChar;
+        String rawContent = "";
+        String content = "";
         List<Course> courses = null;
         courses = courseService.getStudentCourses(student, isFinished);
+        if (!courses.isEmpty()) {
+            for (int i = 0; i < courses.size(); i++) {
+                contentStr = courses.get(i).getContent();
+                contentPart = contentStr.split("><");
+                lastChar = contentPart[0].charAt(contentPart[0].length() - 1);
+
+                if (String.valueOf(lastChar).equalsIgnoreCase("p")) {
+                    rawContent = contentPart[0] + ">";
+                    if (rawContent.length() >= 255) {
+                        content = rawContent.substring(0, 255) + "...";
+                    } else {
+                        content = rawContent;
+                    }
+                } else {
+                    rawContent = contentPart[0];
+                    if (rawContent.length() >= 255) {
+                        content = rawContent.substring(0, 255) + "...";
+                    } else {
+                        content = rawContent;
+                    }
+                }
+
+                courses.get(i).setContent(content);
+
+            }
+        }
+
+        String userImgUrl = userDetails.getUser().getImageURL();
+
+        model.addAttribute("userImgUrl", userImgUrl);
 
         model.addAttribute("userName", userDetails.getUser().getFirstName());
         model.addAttribute("courses", courses);
@@ -89,6 +144,40 @@ public class StudentController {
         return "student/registered-course";
     }
 
+    @GetMapping("/course/{id}/feedback")
+    private String feedback(@PathVariable String id, Model model) {
+        model.addAttribute("id", id);
+        model.addAttribute("user", "student");
+
+        return "student/feedback";
+    }
+
+    @ModelAttribute("feedback")
+    public FeedbackDto feedbackDto() {
+        return new FeedbackDto();
+    }
+
+    @PostMapping("/course/{id}/feedback")
+    private String giveFeedback(@PathVariable String id,
+            @ModelAttribute("feedback") FeedbackDto feedbackDto, Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        CustomUserDetailsService userDetails = (CustomUserDetailsService) auth.getPrincipal();
+
+        model.addAttribute("feedback", new FeedbackDto());
+        Course course = courseService.getCourseById(Long.parseLong(id)).get();
+        Student student = studentService.getStudentInfo(userDetails.getUser());
+        Boolean isEnrolled = courseService.isAlreadyEnrolled(course, student);
+        model.addAttribute("userName", userDetails.getUser().getFirstName());
+        model.addAttribute("course", course);
+        model.addAttribute("isEnrolled", isEnrolled);
+        model.addAttribute("user", "student");
+
+        feedbackService.createNewFeedback(feedbackDto, student, course);
+
+        // System.out.println(top4Course.getContent().get(0).getImageUrl());
+        return "student/feedback-successful";
+    }
+
     @GetMapping("/course/{id}")
     private String courseInformation(@PathVariable String id, Model model) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
@@ -97,10 +186,15 @@ public class StudentController {
         Course course = courseService.getCourseById(Long.parseLong(id)).get();
         Student student = studentService.getStudentInfo(userDetails.getUser());
         Boolean isEnrolled = courseService.isAlreadyEnrolled(course, student);
+        String userImgUrl = userDetails.getUser().getImageURL();
+        List<CourseDto> mostLearnCourses = courseService.getMostEnrolledCourses();
 
+        model.addAttribute("userImgUrl", userImgUrl);
         model.addAttribute("userName", userDetails.getUser().getFirstName());
         model.addAttribute("course", course);
         model.addAttribute("isEnrolled", isEnrolled);
+        model.addAttribute("mostLearnCourses", mostLearnCourses);
+        model.addAttribute("user", "student");
 
         // System.out.println(top4Course.getContent().get(0).getImageUrl());
         return "student/course-information";
@@ -133,7 +227,9 @@ public class StudentController {
         Course course = courseService.getCourseById(Long.parseLong(id)).get();
         Student student = studentService.getStudentInfo(userDetails.getUser());
         Boolean isEnrolled = courseService.isAlreadyEnrolled(course, student);
+        String userImgUrl = userDetails.getUser().getImageURL();
 
+        model.addAttribute("userImgUrl", userImgUrl);
         model.addAttribute("userName", userDetails.getUser().getFirstName());
         model.addAttribute("course", course);
 
@@ -143,6 +239,7 @@ public class StudentController {
         model.addAttribute("firebasePrefix", prefix);
         model.addAttribute("firebaseSuffix", suffix);
         model.addAttribute("enrollmentId", studentCourseEnrollmentService.getEnrollmentInfo(student, course).getId());
+        model.addAttribute("user", "student");
 
         if (!isEnrolled) {
             return "redirect:/app/student/course/" + id;
@@ -168,6 +265,7 @@ public class StudentController {
         model.addAttribute("userName", userDetails.getUser().getFirstName());
         model.addAttribute("course", course);
         model.addAttribute("isEnrolled", isEnrolled);
+        model.addAttribute("user", "student");
 
         String prefix = env.getProperty("FIREBASE_PREFIX");
         String suffix = env.getProperty("FIREBASE_SUFFIX");
@@ -186,24 +284,32 @@ public class StudentController {
     }
 
     @GetMapping("/learn/{id}/milestone")
-    private String courseMileStone(@PathVariable String id, Model model) {
+    private String courseMileStone(@PathVariable String id, @RequestParam(name = "week") Integer week,
+            Model model) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         CustomUserDetailsService userDetails = (CustomUserDetailsService) auth.getPrincipal();
 
         Course course = courseService.getCourseById(Long.parseLong(id)).get();
         Student student = studentService.getStudentInfo(userDetails.getUser());
         Boolean isEnrolled = courseService.isAlreadyEnrolled(course, student);
+        StudentCourseEnrollment enroll = studentCourseEnrollmentService.getEnrollmentInfo(student, course);
+        List<MilestoneDto> milestone = courseService.milestone(enroll);
+        String userImgUrl = userDetails.getUser().getImageURL();
 
+        model.addAttribute("userImgUrl", userImgUrl);
         model.addAttribute("userName", userDetails.getUser().getFirstName());
         model.addAttribute("course", course);
         model.addAttribute("isEnrolled", isEnrolled);
+        model.addAttribute("user", "student");
 
         String prefix = env.getProperty("FIREBASE_PREFIX");
         String suffix = env.getProperty("FIREBASE_SUFFIX");
 
         model.addAttribute("firebasePrefix", prefix);
         model.addAttribute("firebaseSuffix", suffix);
-        model.addAttribute("enrollmentId", studentCourseEnrollmentService.getEnrollmentInfo(student, course).getId());
+        model.addAttribute("enrollmentId", enroll.getId());
+        model.addAttribute("milestone", milestone);
+        model.addAttribute("currentWeek", week);
 
         if (!isEnrolled) {
             return "redirect:/app/student/course/" + id;
@@ -212,17 +318,4 @@ public class StudentController {
         // System.out.println(top4Course.getContent().get(0).getImageUrl());
         return "student/milestone";
     }
-
-    @GetMapping("/course/{id}/feedback")
-    private String feedback(@PathVariable String id, Model model) {
-        model.addAttribute("id", id);
-
-        return "student/feedback";
-    }
-
-    @ModelAttribute("feedback")
-    public FeedbackDto feedbackDto() {
-        return new FeedbackDto();
-    }
-
 }
