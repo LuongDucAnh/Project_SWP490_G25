c86f3efe33136632d7ff4f08e2996f396d062324
HuyPXHE140702 <huypxhe140702@fpt.edu.vn>
2022-05-01 00:51:15 +0700

    Update TeacherController.java

--- swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/controller/TeacherController.java
+++ swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/controller/TeacherController.java
@@ -56,7 +56,7 @@ public class TeacherController {
 
         String contentStr = "";
         String[] contentPart;
-        
+
         char lastChar;
         String rawContent = "";
         String content = "";
