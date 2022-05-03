8d474cff7350bc9ec5e50eb135437fffd16be2a7
Nguyễn Trung Thành <97335017+ThanhNT130929@users.noreply.github.com>
2022-05-03 18:58:17 +0700

    Update StudentCourseEnrollmentService.java

--- swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/service/StudentCourseEnrollmentService.java
+++ swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/service/StudentCourseEnrollmentService.java
@@ -5,6 +5,7 @@ import com.example.swp490_g25_sse.model.Student;
 import com.example.swp490_g25_sse.model.StudentCourseEnrollment;
 
 public interface StudentCourseEnrollmentService {
+  
   StudentCourseEnrollment save(StudentCourseEnrollment enrollment); 
 
   StudentCourseEnrollment getEnrollmentInfo(Student student, Course course); 
