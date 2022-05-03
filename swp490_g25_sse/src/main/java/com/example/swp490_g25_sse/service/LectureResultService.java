4415a7cb8e71d7f7946bdd029999652129c5f4d3
Nguyễn Trung Thành <97335017+ThanhNT130929@users.noreply.github.com>
2022-05-03 18:56:56 +0700

    Update LectureResultService.java

--- swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/service/LectureResultService.java
+++ swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/service/LectureResultService.java
@@ -5,6 +5,7 @@ import com.example.swp490_g25_sse.model.LectureResult;
 import com.example.swp490_g25_sse.model.StudentCourseEnrollment;
 
 public interface LectureResultService {
+  
   LectureResult createLectureReport(StudentCourseEnrollment enrollment, Lecture lecture);
 
   LectureResult updateLectureResult(StudentCourseEnrollment enrollment, Lecture lecture, Boolean isFinished);
