ca7ce4aa0bbbdd0a9dbd0a898dd9c2dc0b517534
Nguyễn Trung Thành <97335017+ThanhNT130929@users.noreply.github.com>
2022-05-03 18:53:54 +0700

    Update TeacherRepository.java

--- swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/repository/TeacherRepository.java
+++ swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/repository/TeacherRepository.java
@@ -12,5 +12,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @author msi
  */
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+    
     Teacher findFirstByUserId(long id);
 }
