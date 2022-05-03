99fc32660998e1346026842f70f4d9d028e45f4c
Nguyễn Trung Thành <97335017+ThanhNT130929@users.noreply.github.com>
2022-05-03 18:52:42 +0700

    Update AnswerRepository.java

--- swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/repository/AnswerRepository.java
+++ swp490_g25_sse/src/main/java/com/example/swp490_g25_sse/repository/AnswerRepository.java
@@ -7,5 +7,6 @@ import com.example.swp490_g25_sse.model.Answer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
+    
     List<Answer> findByQuestionId(long id);
 }
